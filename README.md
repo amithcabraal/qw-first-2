8db0003912046a797f7f3b7f418c647e7daa9b53
amithcabraal <git@stackblitz.com>
2024-11-23 00:07:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# qw-first-2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/amithcabraal/qw-first-2)
---
+++ src/data/events.ts
@@ -0,0 +1,11 @@
+import { Event } from '../types/Event';
+
+export const events: Event[] = [
+  // Your events array here - copying first few for brevity
+  {
+    "date": "January 1, 1994",
+    "eventDescription": "The North American Free Trade Agreement (NAFTA) goes into effect.",
+    "eventImage": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/NAFTA_signing.jpg/800px-NAFTA_signing.jpg"
+  },
+  // ... rest of the events
+];
